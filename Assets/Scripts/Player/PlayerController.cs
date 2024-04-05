6e95be2fdc70f380e3b1908b2466476e95abb6e1
William_Yranib <a729121423@gmail.com>
2024-04-05 22:16:41 +0800

    角色移动和翻转

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl InputControl;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
     public Vector2 inputDirection;
+    public float speed;
     private void Awake()
     {
         InputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -26,12 +31,27 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         inputDirection = InputControl.Gameplay.Move.ReadValue<Vector2>();
-    } 
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime,rb.velocity.y);
+        if (inputDirection.x != 0)
+        {
+            spriteRenderer.flipX = inputDirection.x > 0 ? false : true;
+        }
+    }
 }
